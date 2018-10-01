6bdd1c30ff81996980468366f5173bc6b0ffd615
Hayen Phan <hayen@MacBook-Pro-van-Hayen.local>
2018-10-01 11:27:59 +0200

    Added javascript

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,12 @@
 const getMessage = () => {
-    console.log('yo')
+    console.log('testesttestest')
 }
 
 getMessage();
+
+window.addEventListener('scroll', () => {  // Triggering the scroll event
+  const scrolled = window.pageYOffset;  // Returns the numbers of pixels for scrolling
+  const image = document.querySelector('.intro-section'); // Select the image
+  image.style.top = scrolled * 0.9 + 'px'; // Top position image = numbers of pixels while scrolling * 0.9
+  console.log(image)
+})
