d4096891bf4226a2f1ce8f241b1dd317c63e1a44
Hayen Phan <hayen@MacBook-Pro-van-Hayen.local>
2018-10-03 20:59:33 +0200

    Changed variables

--- src/server/app.js
+++ src/server/app.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 // viewed at http://localhost:8080
 app.use(express.static('src/server'))
