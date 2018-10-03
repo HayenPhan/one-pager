bd65e9689854ad7a22e09d945bbe3f53a21210a2
Hayen Phan <hayen@MacBook-Pro-van-Hayen.local>
2018-10-03 20:39:17 +0200

    Fixed app.js

--- src/server/app.js
+++ src/server/app.js
@@ -1,11 +1,8 @@
 var express = require('express');
 var app = express();
-var path = require('path');
 
 // viewed at http://localhost:8080
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
+app.use(express.static('src/server'))
 
 app.listen(8080);
 
