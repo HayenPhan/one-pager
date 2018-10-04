8709362319af37141c63add821433ab1ab4eb8a3
Hayen Phan <hayen@MacBook-Pro-van-Hayen.local>
2018-10-04 12:51:58 +0200

    Changed position files

--- src/server/index.js
+++ src/server/index.js
@@ -1,15 +1,8 @@
-const getMessage = () => {
-    console.log(a);
-}
-
-getMessage();
-
 window.addEventListener('scroll', () => {  // Triggering the scroll event
   const scrolled = window.pageYOffset;  // Returns the numbers of pixels for scrolling
   const image = document.querySelector('.section__first'); // Select the image
 
   image.style.top = scrolled * 0.9 + 'px'; // Top position image = numbers of pixels while scrolling * 0.9
-  console.log(image)
 })
 
 
