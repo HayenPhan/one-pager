f1f9c464e567ceaacac43a84e2889e62789b7e58
Hayen Phan <hayen@MacBook-Pro-van-Hayen.local>
2018-10-04 11:16:31 +0200

    Changed src

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const sass = require('gulp-sass');
 gulp.task('styles', () => {
   return gulp.src('src/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('src/css/'));
+    .pipe(gulp.dest('src/server/css/'));
 });
 
 // Watch task
